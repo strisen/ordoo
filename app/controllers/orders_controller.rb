cb8ef88cc35bb61bdd27b55fa6d322c21e1702c6
Wong Kin Ting <strisen@Wongs-MacBook-Pro.local>
2018-02-19 23:36:30 +0800

    Submit Version with customised JSON views

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,11 +1,12 @@
 class OrdersController < ApplicationController
   def index
-    @orderList = DeliveryOrder.all
-    render json: @orderList
+    @orderList = DeliveryOrder.all.as_json
+    render json: @orderList.as_json(:except => [:order_items])
   end
 
   def show
-    @currentOrder = DeliveryOrder.find(params[:id])
+    @currentOrder = DeliveryOrder.find(params[:id]).as_json
     render json: @currentOrder
   end
+
 end
--- app/models/delivery_order.rb
+++ app/models/delivery_order.rb
@@ -1,3 +1,19 @@
 class DeliveryOrder < ApplicationRecord
   has_many :order_item
+
+  def as_json(options={})
+    @orderItems = OrderItem.where(delivery_order: self.id).map do |item|
+      {
+        name: Meal.find(item.meal_id).name,
+        quantity: item.quantity,
+        total_price: item.quantity * item.unit_price
+      }
+    end
+    json = {
+      :order_id => self.order_id,
+      :delivery_date => self.serving_datetime.to_date,
+      :delivery_time => self.serving_datetime.strftime('%H:%M%p') + "-" + (self.serving_datetime + 30*60).strftime('%H:%M%p'),
+      :order_items => @orderItems
+    }
+  end
 end
